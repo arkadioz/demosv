b7dc8153e1b9c39f309e43ae3f200ec8cd155416
unknown <davv913@hotmail.com>
2023-12-27 12:35:07 -0500

    feat: first version of the project

---
+++ src/main/java/com/davv/demosv/DemosvApplication.java
@@ -0,0 +1,13 @@
+package com.davv.demosv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosvApplication.class, args);
+	}
+
+}
